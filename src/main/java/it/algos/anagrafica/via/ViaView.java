4935992838cc144aeaa5b5acad4753f00c007462
gac <info@algos.it>
2024-06-22 15:38:59 +0200

    menu

--- src/main/java/it/algos/anagrafica/via/ViaView.java
+++ src/main/java/it/algos/anagrafica/via/ViaView.java
@@ -2,7 +2,6 @@ package it.algos.anagrafica.via;
 
 import com.vaadin.flow.router.Route;
 import it.algos.vbase.backend.annotation.AView;
-import it.algos.vbase.backend.constant.Gruppo;
 import it.algos.vbase.ui.view.CrudView;
 import it.algos.vbase.ui.view.MainLayout;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Route chiamata dal menu generale o dalla barra del browser <br>
  */
 @Route(value = "via", layout = MainLayout.class)
-@AView(menuName = "Vie", menuGroup = Gruppo.ANAG)
+@AView(menuName = "Vie")
 public class ViaView extends CrudView {
 
     /**
